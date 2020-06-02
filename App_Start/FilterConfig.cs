22d271720d03138fb16bed9568520675c4e264a0
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-06-01 23:45:52 -0500

    firt commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Huella_de_Carbono
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Controllers/CalculoController.cs
@@ -0,0 +1,25 @@
+﻿using Huella_de_Carbono.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Huella_de_Carbono.Controllers
+{
+    public class CalculoController : Controller
+    {
+        // GET: Calculo
+        public ActionResult Index()
+        {
+            using (var db = new HuelladeCarbonoEntities1())
+            {
+
+                var list = db.usuario.ToList();
+                return View(list);
+
+            }
+                
+        }
+    }
+}
