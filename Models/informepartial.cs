7917a4d04e837dc75b3bda284428211c31eb2f16
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-06-12 23:47:50 -0500

    completado estilo y preguntas de la calculadora

--- Models/informepartial.cs
+++ Models/informepartial.cs
@@ -22,7 +22,12 @@ namespace Huella_de_Carbono.Models
 
     public partial class informes
     {
+        [Display(Name = "Cuantas veces consume carne a la semana")]
+
+        public double meat { get; set; }
+
         [Display(Name ="Cuanto tiempo pasa en autobus")]
+        
         public double bustime { get; set; }
 
         [Display(Name = "Cuanto tiempo pasa en carro")]
@@ -31,6 +36,7 @@ namespace Huella_de_Carbono.Models
         [Display(Name = "Cuanto tiempo pasa en moto")]
         public double mototime { get; set; }
 
+        [Display(Name ="INTENTOS")]
         public double intentos { get { return CalculoController.numcantidad(); } }
 
         [Display(Name = "Promedio")]
@@ -38,7 +44,7 @@ namespace Huella_de_Carbono.Models
 
         //public String NombreCompleto {get {return Nombres + " " + Apellidos; } }
         [Display (Name ="Resultado")]
-        public double total { get { return (bustime * 0.1) + (cartime* 0.3) + (mototime * 0.2); }}
-
+        public double total { get { return (bustime * 0.1) + (cartime* 0.3) + (mototime * 0.2) + (meat*2.9); }}
+        //public double total { get { return meat; } }
     }
 }
